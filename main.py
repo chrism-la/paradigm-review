bbba93f17043aeef308fb4cd0a7083cc2da939ff
Christopher <117781333+chrism-la@users.noreply.github.com>
2024-03-15 05:18:06 -0700

    prompts

---
+++ main.py
@@ -0,0 +1,30 @@
+#Functional Programming Prompt
+'''Implement a function that will flatten and sort an array of integers in ascending order, and which adheres to a functional programming paradigm.'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Object Oriented Prompt
+'''
+Watto needs a new system for organizing his inventory of podracers. Help him do this by implementing an Object Oriented solution according to the following criteria.
+
+First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes.
+Define a repair() method that will update the condition of the podracer to "repaired".
+Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
+Define another class that inherits Podracer and call this one SebulbasPod. This class should have a special method called flame_jet that will update the condition of another podracer to "trashed".
+'''
