8ecbf586cba932015c0fe959cb350616ad1e60ff
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-20 14:58:38 -0700

    added all methods to classes

--- oop.py
+++ oop.py
@@ -12,10 +12,21 @@ class Podracer:
     self.condition = condition
     self.price = price
 
+  def repair(self):
+    self.condition = "repaired"
+
+
 class AnakinsPod(Podracer):
   def __init__(self, max_speed, condition, price):
     super().__init__(max_speed,condition,price)
 
+  def boost(self):
+    self.max_speed *= 2
+    
+
 class SebulbasPod(Podracer):
   def __init__(self,max_speed,condition,price):
     super().__init__(max_speed,condition,price)
+
+  def flame_jet(self, other_podracer):
+    other_podracer.condition = "trashed"
