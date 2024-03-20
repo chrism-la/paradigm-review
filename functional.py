eb70a10150c0040028cae6f286e40e37f8a7bab3
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-20 15:19:27 -0700

    answered functional prompt questions

--- functional.py
+++ functional.py
@@ -14,10 +14,15 @@ def asc_order(arr):
   return result
   ''' 
 #Flattening using List comprehension
+  '''
   result = [element for subarr in arr for element in subarr]
   result.sort()
   return result
-  
+  '''
+#Returning new sorted list as opposed to modifying the original
+  result = [element for subarr in arr for element in subarr]
+  sorted_list = sorted(result)
+  return sorted_list
 
 arr1 = [[1,2,4,7],[5,6,3]]
 arr2 = [["hello","my"],["friend"]]
@@ -26,6 +31,23 @@ print(asc_order(arr2))
     
 
 
+'''
+How does this solution ensure data immutability?
+  It ensures data immutability by returning a new list rather than modifying the original.
+
+Is this solution a pure function? Why or why not?
+  Yes this is a pure function because given the same input the function will always return the same output.
+
+Is this solution a higher order function? Why or why not?
+  No it is not a higher order function because it does not take any other functions as arguments or returns a function as its result.
+
+Would it have been easier to solve this problem using a different programming style?
+  Functional programming is better for this problem because it leads to a concise and readable solution.
+
+Why in particular is functional programming a helpful paradigm when solving this problem?
+  Functional programming provides better tools such as list comprehensions.
+
+'''
 
 
 
