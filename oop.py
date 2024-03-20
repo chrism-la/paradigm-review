d0c9b7b40e5f977d9ff169338f228a21d5a5582b
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-20 14:54:35 -0700

    (OOP) defined two classes one named Podracer and one that inherits from Podracer named AnakinsPodf

---
+++ oop.py
@@ -0,0 +1,17 @@
+#Object Oriented Programming prompt 
+'''
+First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes.
+Define a repair() method that will update the condition of the podracer to "repaired".
+Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
+Define another class that inherits Podracer and call this one SebulbasPod. This class should have a special method called flame_jet that will update the condition of another podracer to "trashed"
+'''
+
+class Podracer:
+  def __init__(self, max_speed, condition, price):
+    self.max_speed = max_speed
+    self.condition = condition
+    self.price = price
+
+class AnakinsPod(Podracer):
+  def __init__(self, max_speed, condition, price):
+    super().__init__(max_speed,condition,price)
